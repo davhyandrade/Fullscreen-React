bab17f4a4093f03425ad1170c9b01876db81278f
Davhy Andrade <davhyhello@gmail.com>
2022-09-09 19:39:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Carousel from './components/Carousel'
+import './components/css/style.css'
+
+export default () => {
+
+  return (
+    <>
+      <Carousel />
+    </>
+  )
+}
